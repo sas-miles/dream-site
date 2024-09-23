12456258aec47c291f06d28761b6a88edb354467
sas-miles <132398659+sas-miles@users.noreply.github.com>
2024-09-23 17:34:10 -0400

    fixing landing ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Landing from "~/_components/intro-scene/Landing";
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-black">
-      <div className="fixed inset-0 z-0">
+      <div className="fixed inset-0 z-50 h-screen w-full">
         <Landing />
       </div>
     </div>
