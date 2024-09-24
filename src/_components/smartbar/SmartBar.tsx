c6346736750e8c4944f98dd4e84fd8ec366f4c0e
sas-miles <132398659+sas-miles@users.noreply.github.com>
2024-09-24 12:06:59 -0400

    Drone page static layout

--- src/_components/smartbar/SmartBar.tsx
+++ src/_components/smartbar/SmartBar.tsx
@@ -5,29 +5,21 @@ import { useSmartBarStore } from "~/store/smartbarStore";
 import { motion } from "framer-motion";
 import ClosedSmartBar from "./ClosedSmartBar";
 import OpenSmartBar from "./OpenSmartBar";
-import ContextSideBar from "./ContextSideBar";
+import { usePathname } from "next/navigation";
 
 function SmartBar() {
-  const isOpen = useSmartBarStore((state) => state.isOpen);
-  const toggleOpen = useSmartBarStore((state) => state.toggleOpen);
+  const pathname = usePathname();
 
-  const handleToggleContextSideBar = () => {
-    // Your logic to toggle the context sidebar
-  };
+  const isOpen = useSmartBarStore((state) => state.isOpen);
+  if (pathname === "/experience") return null;
 
   return (
     <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
     >
       {isOpen ? <OpenSmartBar /> : <ClosedSmartBar />}
-      <ContextSideBar
-        isOpen={isOpen}
-        onClose={toggleOpen}
-        onToggleContextSideBar={toggleOpen}
-        // ... other props
-      />
     </motion.div>
   );
 }
